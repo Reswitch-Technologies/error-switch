3530a1a376f91986501b9dab9ebd01ca85da0655
Saunved <saunved@gmail.com>
2021-03-13 19:30:24 +0530

    feat: to simplify Intellisense identification, I've moved the 'name' variable out to be the key of the error type

--- index.js
+++ index.js
@@ -1,53 +1,43 @@
 export default {
-  errors: {
-    AUTHENTICATION: {
-      A1: {
-        name: 'LOGIN_FAILED',
-        description: 'Login failed because the username or password was wrong',
-      },
-      A2: {
-        name: 'ACCOUNT_ALREADY_EXISTS',
-        description: 'Registration failed because the account already exists',
-      },
+  AUTHENTICATION: {
+    LOGIN_FAILED: {
+      description: 'Login failed because the username or password was wrong',
     },
-    DATABASE: {
-      D1: {
-        name: 'UNKNOWN_ERROR',
-        description: 'An error was thrown by the database, but no cause was found',
-      },
-      D2: {
-        name: 'KNOWN_ERROR',
-        description: 'An error was thrown by the database with an explanation',
-      },
-    },
-    LIMIT: {
-      L1: {
-        name: 'OTP_REQUEST_LIMIT',
-        description: 'The user requested for the OTP too many times',
-      },
-      L2: {
-        name: 'OTP_CONFIRM_LIMIT',
-        description: 'The user tried to confirm the OTP too many times',
-      },
-    },
-    VALIDATION: {
-      V1: {
-        name: 'EMAIL_FAILED',
-        description: 'Not a valid email ID',
-      },
-      V2: {
-        name: 'PASSWORD_WEAK',
-        description: 'Password strength is too weak',
-      },
+    ACCOUNT_ALREADY_EXISTS: {
+      description: 'Registration failed because the account already exists',
     },
   },
-
+  DATABASE: {
+    UNKNOWN_ERROR: {
+      description: 'An error was thrown by the database, but no cause was found',
+    },
+    KNOWN_ERROR: {
+      description: 'An error was thrown by the database with an explanation',
+    },
+  },
+  LIMIT: {
+    OTP_REQUEST_LIMIT: {
+      description: 'The user requested for the OTP too many times',
+    },
+    OTP_CONFIRM_LIMIT: {
+      description: 'The user tried to confirm the OTP too many times',
+    },
+  },
+  VALIDATION: {
+    EMAIL_FAILED: {
+      description: 'Not a valid email ID',
+    },
+    PASSWORD_WEAK: {
+      description: 'Password strength is too weak',
+    },
+  },
 };
 
 /*
 Example error format
 {
-  error: 'A1',
+  error: 'EMAIL_FAILED',
+  errorType: 'VALIDATION,
   message: '...'
 }
 */
